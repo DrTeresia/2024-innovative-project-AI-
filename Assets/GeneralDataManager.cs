61f2e60016e565433909028c54ca027695c355dc
12211316 <12211316@mail.sustech.edu.cn>
2025-06-02 15:36:25 +0800

    选人物功能

---
+++ Assets/GeneralDataManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class GeneralDataManager
+{
+    // 存储玩家选择的英雄名称
+    public static string SelectedHeroName { get; set; }
+}
